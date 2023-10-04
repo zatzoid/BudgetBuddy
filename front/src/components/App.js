d256581f2763efb582d18900f8454332442e51f6
george <zatzoid.gmail.com>
2023-10-04 21:21:29 +0300

    qwe

---
+++ front/src/components/App.js
@@ -0,0 +1,11 @@
+import Header from "./header/Header"; /* подключаем компонент */
+
+function App() {
+  return (
+    <div className="App">
+      <Header /> {/* отображаем компонент */}
+    </div>
+  );
+}
+
+export default App;
---
+++ front/src/components/header/Header.js
@@ -0,0 +1,9 @@
+/* экспортируем компонент */
+export default function Header() {
+    return (
+        <div className="header">
+            <h1 className="header__heading ">budegetasdas</h1>
+        </div>
+    );
+}
+
