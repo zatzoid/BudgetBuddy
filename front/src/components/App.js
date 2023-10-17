506e9696440587f5c7b36594eafca41bfec66dd1
zatzoid <zatzoid@gmail.com>
2023-10-17 20:44:10 +0300

    footer/header

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -1,9 +1,23 @@
-import Header from "./header/Header"; /* подключаем компонент */
+import React, { useEffect, useState } from "react";
+import Header from "./header/Header";
+import Footer from "./footer/footer";
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  function log() {
+    if (!loggedIn) {
+      console.log(true)
+      return setLoggedIn(true)
+
+
+    }
+    console.log(false)
+    setLoggedIn(false)
+  }
   return (
     <div className="App">
-      <Header /> {/* отображаем компонент */}
+      <Header loggedIn={loggedIn} log={log} />
+      <Footer />
     </div>
   );
 }
--- front/src/components/header/Header.js
+++ front/src/components/header/Header.js
@@ -1,9 +1,53 @@
-/* экспортируем компонент */
-export default function Header() {
+import React, { useEffect, useState } from "react";
+import { NavLink } from 'react-router-dom'
+
+
+export default function Header(props) {
+    const [showMenu, setShowMenu] = useState(false);
+    const [showNavBar, setShowNavBar] = useState(false)
+    useEffect(() => {
+        setTimeout(() => {
+            if (showNavBar) {
+                return setShowNavBar(false)
+            }
+            setShowNavBar(true)
+        }, 500);
+
+    }, [showMenu])
+    function openMenu() {
+        if (showMenu) {
+            return setShowMenu(false)
+        }
+        setShowMenu(true);
+    }
     return (
-        <div className="header">
-            <h1 className="header__heading ">budegetasdas</h1>
-        </div>
+        <header className='header' >
+            <div className="header__content">
+                <h1 onClick={() => { props.log() }}
+                    className={`header__heading ${!props.loggedIn && 'header__heading_unlogged '}`}>
+                    BudgetBuddy</h1>
+                {
+                    props.loggedIn &&
+                    <button className="haeder__navbar-btn" onClick={() => { openMenu() }}>
+                        <hr className={`header__navbar-btn-line ${showMenu && 'header__navbar-btn-line_top'}`} />
+                        <hr className={`header__navbar-btn-line ${showMenu && 'header__navbar-btn-line_center'}`} />
+                        <hr className={`header__navbar-btn-line ${showMenu && 'header__navbar-btn-line_bottom'}`} />
+                    </button>
+                }
+            </div>
+            <div className={`header__navbar-wrapper ${showMenu && 'header__navbar-wrapper_active'}`}>
+                <nav className="header__navbar">
+                    <NavLink to='/local-posts' className='header__link'>
+                        Личные посты</NavLink>
+                    <NavLink to='/public-posts' className='header__link'>
+                        Общие посты</NavLink>
+
+                </nav>
+                <button className="header__acc-btn">Выйти из аккаунта</button>
+                <button className="header__acc-btn">Удалить аккаунт</button>
+            </div>
+
+        </header >
     );
 }
 
