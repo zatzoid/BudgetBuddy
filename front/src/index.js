1e52f3031d47931ad156707199e7d33d17e54a5d
zatzoid <zatzoid@gmail.com>
2024-04-06 18:13:21 +0300

    hz

--- front/src/index.js
+++ front/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App';
+import App from './App.js';
 import reportWebVitals from './reportWebVitals';
 
 
